a479af404ec7e73a1c92e38b8f88ef4980bd377f
Sai Harsha <81852274+Saiharshakosaraju@users.noreply.github.com>
2023-05-16 18:28:47 +0530

    Update TestRun.cpp

--- TestRun.cpp
+++ TestRun.cpp
@@ -1,18 +1,20 @@
 #include<bits/stdc++.h>
 #include "./linkedList.hpp"
 using namespace std;
+using namespace dsa;
 
 int main(){
-    dsa::LinkedList<int> List1;
-    dsa::LinkedList<int> List2;
-    List1.beginInsert(1);
-    List1.beginInsert(2);
-    List1.beginInsert(3);
-    List1.beginInsert(4);
-    List1.beginInsert(5);
-    List2.beginInsert(6);
-    List2.beginInsert(7);
+    LinkedList<int> List1;
+    LinkedList<int> List2;
+    List1.beginInsert(21);
+    List1.beginInsert(75);
+    List1.beginInsert(50);
+    List2.beginInsert(11);
+    List2.beginInsert(32);
     List1.display();
     List2.display();
+    List1.isSorted();
+    List1.sort();
+    List1.display();
     return 0;
 }
